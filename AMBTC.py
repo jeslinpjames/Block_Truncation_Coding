c0d7d544cfdda12d60a344a294863d158d236899
jeslinpjames <jeslinpjames007@gmail.com>
2023-12-12 07:56:27 +0530

    Functions added

---
+++ AMBTC.py
@@ -0,0 +1,30 @@
+import cv2
+from bitarray import bitarray
+import numpy as np
+
+def load_image(path):
+    try:
+        img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+        if img is not None:
+            return img
+        else:
+            print("Image not found")
+            return None
+    except Exception as e:
+        print("Error in loading image")
+        print(e)
+        return None
+    
+def display_image(img):
+    if img is not None:
+        cv2.imshow("image",img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    else:
+        print("Image not found")
+
+def save_image(img,path):        
+    if img is not None:
+        cv2.imwrite(path,img)
+    else:
+        print("Image not found")
