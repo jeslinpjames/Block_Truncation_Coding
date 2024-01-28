e4f29c926dcaa59a725e2850452b18d476726bea
jeslinpjames <jeslinpjames007@gmail.com>
2024-01-28 09:22:03 +0530

    saving image with PIL

--- add_speckle.py
+++ add_speckle.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from PIL import Image
 
 def add_speckle_noise(image, intensity):
     if len(image.shape) == 2:
@@ -17,9 +18,44 @@ def add_speckle_noise(image, intensity):
     else:
         raise ValueError("Unsupported image shape")
 
+
+def save_image(img, path):
+    if img is not None:
+        pil_img = Image.fromarray(img)
+        pil_img = pil_img.convert('1')
+        pil_img.save(path, 'BMP', bits=1)
+    else:
+        print("Image not found")
+
+# def main():
+#     # Load an image
+#     img = cv2.imread("D:/git/Block_Truncation_Coding/images/synthetic.bmp")
+
+#     # Convert the image to grayscale
+#     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+#     # Define a list of intensities for speckle noise
+#     intensity_values = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+
+#     # Apply speckle noise with varying intensities and save the results
+#     for intensity in intensity_values:
+#         noisy_img = add_speckle_noise(img_gray.copy(), intensity)
+
+#         # Convert to 1-bit per pixel
+#         _, noisy_img = cv2.threshold(noisy_img, 127, 255, cv2.THRESH_BINARY)
+
+#         # Save the image as monochrome bitmap in BMP format using Pillow
+#         output_path = f'img/noisy_image_intensity_{intensity}.bmp'
+        
+#         pil_img = Image.fromarray(noisy_img)
+#         pil_img = pil_img.convert('1')
+#         pil_img.save(output_path, 'BMP', bits=1)
+#         Image.fromarray(noisy_img).save(output_path, compression='RLE')
+#         print(f'Image saved: {output_path}')
+
 def main():
     # Load an image
-    img = cv2.imread("D:/git/Block_Truncation_Coding/images/lena.png")
+    img = cv2.imread("D:/git/Block_Truncation_Coding/images/synthetic.bmp")
 
     # Convert the image to grayscale
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -30,9 +66,12 @@ def main():
     # Apply speckle noise with varying intensities and save the results
     for intensity in intensity_values:
         noisy_img = add_speckle_noise(img_gray.copy(), intensity)
-        output_path = f'img/noisy_image_intensity_{intensity}.png'
-        cv2.imwrite(output_path, noisy_img)
+
+        # Save the image as monochrome bitmap in BMP format using our custom function
+        output_path = f'img/noisy_image_intensity_{intensity}.bmp'
+        save_image(noisy_img, output_path)
         print(f'Image saved: {output_path}')
+
 
 if __name__ == "__main__":
     main()
