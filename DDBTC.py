f425b388a654fc860b9ee16c6dd4bb9a2f49a251
jeslinpjames <jeslinpjames007@gmail.com>
2024-02-19 19:04:52 +0530

    added functions to DDBTC

---
+++ DDBTC.py
@@ -0,0 +1,40 @@
+import numpy as np
+from skimage.util import view_as_blocks
+import cv2
+from bitarray import bitarray
+from PIL import Image
+
+def load_image(path):
+    try:
+        img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+        if img is not None:
+            return img
+        else:
+            print("Image not found")
+            return None
+    except Exception as e:
+        print("Error in loading image")
+        print(e)
+        return None
+    
+def display_image(img):
+    if img is not None:
+        cv2.imshow("image",img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    else:
+        print("Image not found")
+
+def save_image(img, path):
+    if img is not None:
+        pil_img = Image.fromarray(img)
+        pil_img = pil_img.convert('L')
+        pil_img.save(path, 'BMP', bits=8)
+    else:
+        print("Image not found")
+
+def to_char(value):
+    if 0 <= value <= 255:
+        return value.to_bytes(1, byteorder='big')
+    else:
+        raise ValueError("Value must be in the range 0-255")
